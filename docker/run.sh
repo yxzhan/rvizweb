e5d8ece9a9c0ff13230e449f4062ca2f9893cfeb
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-03-01 16:57:14 +0100

    update config

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run --network="host" -d --privileged --rm -it "yxzhan/rvizweb:noetic"
+docker run --network="host" -d --privileged -it "yxzhan/rvizweb:noetic"
+# sudo apt install -y ros-noetic-interactive-marker-tutorials
